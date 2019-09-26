f96d7947a488fd76e53b87e6a4d70eab903d9c4a
Jakob Stendahl <14180120+JakobST1n@users.noreply.github.com>
2019-09-26 10:58:49 +0200

    Update install_mac.sh

--- bin/install_mac.sh
+++ bin/install_mac.sh
@@ -15,13 +15,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 echo "> Install powerlevel9k"
 brew tap sambadevi/powerlevel9k
 
-echo "Install our dotfiles"
-ln -i tmux.conf ~/.tmux.conf
-ln -i bashrc ~/.bashrc
-ln -i vim/vimrc ~/.vimrc
-ln -i Hyperterm/hyper.js ~/.hyper.js
+echo "> Install our dotfiles"
+ln -i mac/zshrc ~/.zshrc
+ln -i mac/tmux.conf ~/.tmux.conf
+ln -i mac/Hyperterm/hyper.js ~/.hyper.js
+ln -i mac/Hyperterm/local/ ~/.hyper_plugins/local/
 
-ln -i Hyperterm/local ~/.hyper_plugins/local
-
-
-echo "Please install the font Roboto mono nerd, and enable it in your terminal."
+echo "\nPlease install the font Roboto mono nerd, and enable it in your terminal."
