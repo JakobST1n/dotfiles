8724ab1841f647603287d23f3f566a066143bcf0
jakob.stendahl <jakob.stendahl@infomedia.dk>
2022-10-11 12:37:09 +0200

    Fix zsh-vi-mode install

--- install/install_mac.sh
+++ install/install_mac.sh
@@ -46,7 +46,7 @@ if [ $res -eq 1 ]; then
     # Zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     # Zsh-vi-mode
-    git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+    git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
 
     tput rc; tput ed
 fi
