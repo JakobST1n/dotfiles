8dad8baada811d6872f06211d8ee45c2ed9b337a
jakob.stendahl <jakob.stendahl@infomedia.dk>
2024-04-27 15:43:05 +0200

    Remove rust-tools

--- Common/nvim/lua/lsp-conf.lua
+++ Common/nvim/lua/lsp-conf.lua
@@ -15,11 +15,6 @@ require("mason-lspconfig").setup_handlers {
     function (server_name) -- default handler (optional)
         require("lspconfig")[server_name].setup {}
     end,
-
-    ["rust_analyzer"] = function ()
-        require("rust-tools").setup {}
-    end,
-
 }
 
 -- LSP Diagnostics Options Setup
